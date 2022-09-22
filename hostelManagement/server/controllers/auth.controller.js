c5468289b6930b019fdbc8856ded448921fb0d11
vinayKumar3000 <vurukondavinaykumar@gmail.com>
2022-09-22 19:09:56 +0530

    Evaluation 1 final

--- hostelManagement/server/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config/config');
-
-function generateToken(user) {
-    const payload = JSON.stringify(user);
-    return jwt.sign(payload, config.jwtSecret);
-}
-
-module.exports = { generateToken };
